7a6f42994588374e55d73dbf395759c4f49032f1
Diane1505 <dianeblinnouveau@yahoo.fr>
2021-05-25 12:58:44 +0200

    Itinial version

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -21,11 +21,11 @@ const SocialNetwork = () => {
   return (
     <div className="social-network">
       <ul className="content">
-        <a href="" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
-          <li><i className="fab fa-linkedin"></i></li>
-        </a>
         <a href="https://github.com/Diane1505" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
           <li><i className="fab fa-github"></i></li>
+        </a>
+        <a href="https://github.com/Diane1505" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
+          <li><i className="fab fa-linkedin"></i></li>
         </a>
       </ul>
     </div>
